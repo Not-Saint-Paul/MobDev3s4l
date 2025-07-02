ec0e927fb3c1f85c0b4238f654246af55e847fed
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2025-07-02 22:42:15 +0700

    Добавлены элементы для показа цветов и их названий на английском

--- app/src/main/java/com/example/mobdev_3s_4l/MainActivity.kt
+++ app/src/main/java/com/example/mobdev_3s_4l/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: Adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById(R.id.rView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val colorList = ArrayList(arrayListOf(
+            ColorData("Black", 0xFF000000),
+            ColorData("Blue", 0xFF0000FF),
+            ColorData("Lime", 0xFF00FF00),
+            ColorData("Aqua", 0xFF00FFFF),
+            ColorData("Navy", 0xFF000080),
+            ColorData("Green", 0xFF008000),
+            ColorData("Teal", 0xFF008080),
+            ColorData("Maroon", 0xFF800000),
+            ColorData("Purple", 0xFF800080),
+            ColorData("Still Olive", 0xFF808000),
+            ColorData("Grey", 0xFF808080),
+            ColorData("Silver", 0xFFC0C0C0),
+            ColorData("Red", 0xFFFF0000),
+            ColorData("Fuchsia", 0xFFFF00FF),
+            ColorData("Yellow", 0xFFFFFF00),
+            ColorData("White", 0xFFFFFFFF),
+            ColorData("Orange", 0xFFFFA500),
+        ).shuffled())
+
+        adapter = Adapter(this, colorList)
+        recyclerView.adapter = adapter
     }
 }
+
+data class ColorData(
+    var colorName: String,
+    var colorHex: Long,
+)
